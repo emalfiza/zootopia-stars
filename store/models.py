dde5c1eb89940a9443165dbe767fa036f4273c3c
Emal Fiza <emal_fiza@hotmail.com>
2020-06-08 11:17:14 +0000

    Create category and product models 

--- store/models.py
+++ store/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ProductCategory(models.Model):
+    category_name = models.TextField(max_length=50)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    product_category = models.ForeignKey('ProductCategory', null=True, blank=True, on_delete=models.SET_NULL)
+    product_name = models.CharField(max_length=100)
+    product_price = models.DecimalField(max_digits=6, decimal_places=2)
+    product_description = models.TextField(max_length=300)
+    product_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    product_image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.product_name
