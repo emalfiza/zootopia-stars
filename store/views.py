782685a84e02292abb8058d1bc90a9a4fc4f7f3a
Emal Fiza <emal_fiza@hotmail.com>
2020-06-08 18:39:59 +0000

    Add products view and render dynamically on the store page

--- store/views.py
+++ store/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 
 def store(request):
+    """ A view to show all the products and
+    sorting for the store page
+    """
+    products = Product.objects.all()
+
     context = {
+        "products": products,
     }
     return render(request, 'store/store.html', context)
