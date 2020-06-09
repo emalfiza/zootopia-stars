46a2bd9e0d17eadde710c76ff0a69abcbc0db39f
Emal Fiza <emal_fiza@hotmail.com>
2020-06-09 15:11:26 +0000

    Create a category view and url mapping to filter the category

--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import products_by_category_view
 
 urlpatterns = [
     path('', views.store, name="store"),
+    path('category/<category>', products_by_category_view,
+         name='products_by_category'),
 ]
--- store/views.py
+++ store/views.py
@@ -14,3 +14,10 @@ def store(request):
         "products": products,
     }
     return render(request, 'store/store.html', context)
+
+
+def products_by_category_view(request, category):
+    """Filters for product categories"""
+    products = Product.objects.filter(
+        product_category_id__category_name=category)
+    return render(request, "store/store.html", {"products": products})
