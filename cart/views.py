d130274a14253cbe1f385d72587b07ca265377ba
Emal Fiza <emal_fiza@hotmail.com>
2020-06-29 20:06:54 +0000

    Rendering the cart items dynamically

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from order.models import Order
 
 # Create your views here.
 
 
 def cart(request):
+    """ See contents of cart also queries if the user exist
+    otherwise creates one """
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+
     context = {
+        'items': items
     }
     return render(request, 'cart/cart.html', context)
