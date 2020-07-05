5353843d44d8c40236cb8bdd652fdc0b47dc610d
Emal Fiza <emal_fiza@hotmail.com>
2020-07-05 14:32:54 +0000

    Order summary is rendereing dynamically 

--- order/views.py
+++ order/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
+from . models import Order
 
 # Create your views here.
 
 
 def order(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {
+            'get_cart_total': 0,
+            'get_cart_items': 0,
+        }
+
     context = {
+        'items': items,
+        'order': order,
     }
     return render(request, 'order/order.html', context)
